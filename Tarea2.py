4e24e72a371b8779c68a3e3210ef22169874f8b8
crlsr <156555639+crlsr@users.noreply.github.com>
2024-01-13 21:25:26 -0400

    Tarea.py

--- Tarea2.py
+++ Tarea2.py
@@ -1,5 +1,5 @@
 print('INGREDIENTES - AREPA')
-#Creación de la arepa
+#Creación de la arepa.
 agua = int(input("Cuantas tazas de agua?: "))
 harina = int(input("Cuantas tazas de harina?: "))
 sal = float(input("Cuantas cucharadas de sal?: "))
@@ -10,7 +10,7 @@ print(f'Cantidad de arepa {budare}')
 print('Que ingrediente deseas ?: ')
 
 
-#Selección de sabores para la arepa
+#Selección de sabores para la arepa.
 sabor = int(input(('Sabores: \n 1- Jamon \n 2- Carne \n 3- Queso \n 4- Aguacate \n 5- Pollo. \n Elija el número de su ingredient.')))
 if sabor == 1:
     print(f'Su cantidad de arepas {budare} son de Jamon')
