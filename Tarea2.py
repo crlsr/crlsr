ee1576de16938a29f8eeec25b85fa0a717efac14
crlsr <156555639+crlsr@users.noreply.github.com>
2024-01-13 21:23:52 -0400

    Tarea Arepa

---
+++ Tarea2.py
@@ -0,0 +1,24 @@
+print('INGREDIENTES - AREPA')
+
+agua = int(input("Cuantas tazas de agua?: "))
+harina = int(input("Cuantas tazas de harina?: "))
+sal = float(input("Cuantas cucharadas de sal?: "))
+aceite = float(input("Cuantas cucharadas de aceite?: "))
+boul = agua + harina + sal
+budare = aceite + boul 
+print(f'Cantidad de arepa {budare}')
+print('Que ingrediente deseas ?: ')
+#Conversón de =l string a int o float se llama un casting3
+sabor = int(input(('Sabores: \n 1- Jamon \n 2- Carne \n 3- Queso \n 4- Aguacate \n 5- Pollo. \n Elija el número de su ingredient.')))
+if sabor == 1:
+    print(f'Su cantidad de arepas {budare} son de Jamon')
+elif sabor == 2:
+    print(f'Su cantidad de arepas {budare} son de carne')
+elif sabor == 3:
+    print(f'Su cantidad de arepas {budare} son de queso')
+elif sabor == 4:
+    print(f'Su cantidad de arepas {budare} son de aguacate')
+elif sabor == 5:
+    print(f'Su cantidad de arepas {budare} son de pollo')
+else:
+    print('No tiene sabor.')
