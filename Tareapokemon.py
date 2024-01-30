c5bbbe8c395ab4096217dc7a3237a2d3ce999694
crlsr <156555639+crlsr@users.noreply.github.com>
2024-01-30 14:28:08 -0400

    Update Tareapokemon.py

--- Tareapokemon.py
+++ Tareapokemon.py
@@ -1,13 +1,11 @@
 import random
 #Definicion de variables y listas.
-ps_jugador = 100
-ps_oponente = 100
-defensa_oponente = 100
-defensa_jugador = 100
+#Atributos jugador: (vida, defensa)
 
-# (daño, dañodef, nombre ataque)
+atributo_jugador = 100,100
+atributo_oponente = 100,100
 
-#Diccionario ataques (jugador).
+#Diccionario ataques (jugador). Atributos ataques: (daño, dañodef, nombre ataque).
 ataques_jugador = {
     'ataque_1' : (0, 10, 'malicioso'),
     'ataque_2' : (35, 0, 'placaje'),
@@ -24,25 +22,25 @@ elección = int(input(f'Hola! soy {nombre_oponente}, estas listo para esta batal
 if elección == 1:
     print(f'Ok {nombre_jugador} preparate para esta batalla pokemon!')
     print("Tu pokemon es: Charmander \nEl pokemon de tu enemigo es: Squirtle \n")
-    while ps_jugador>0 and ps_oponente>0: 
+    while atributo_jugador[0]>0 and atributo_oponente[0]>0: 
     #Turno Jugador:
         ataque_jugador = input("ataque: ")
         ataque_jugador.lower()
         if ataque_jugador == ataques_jugador['ataque_1'][2]: #malicioso
             print('Has usado malicioso... Un ataque de tipo NORMAL ')
             print('La defensa de squirtle bajado')
-            defensa_oponente = defensa_oponente - ataques_jugador['ataque_1'][1]
-            if defensa_oponente <= 0:
-                defensa_oponente = 1
+            atributo_oponente = atributo_oponente[0], atributo_oponente[1] - ataques_jugador['ataque_1'][1]
+            if atributo_oponente[1] <= 0:
+                atributo_oponente = atributo_oponente[0], 1
         elif ataque_jugador == ataques_jugador['ataque_2'][2]: #placaje
             print('Has usado placaje... Un ataque de tipo NORMAL')
             print('Los ps de Squirtle han bajado')
-            ps_oponente -= ataques_jugador['ataque_2'][0] * (100/defensa_oponente)
+            atributo_oponente = atributo_oponente[0] - ataques_jugador['ataque_2'][0] * (100/atributo_oponente[1]), atributo_oponente[1]
         elif ataque_jugador == ataques_jugador['ataque_3'][2]: #ascuas
             print('Has usado ascuas... Un ataque de tipo FUEGO')
             print('Ataque poco efectivo...')
             print('Los ps de Squirtle han bajado poco')
-            ps_oponente -= ataques_jugador['ataque_3'][0]
+            atributo_oponente = atributo_oponente[0] - ataques_jugador['ataque_3'][0], atributo_oponente[1]
         else:
             print('Que estas haciendo? Tus ataques son placaje, malicioso y ascuas siuuuu' ) 
             continue
@@ -52,28 +50,28 @@ if elección == 1:
             print('Squirtle ha usado latigo... Un ataque de tipo NORMAL')
             print('La defensa de Charmander ha bajado')
             print('Charmander ha perdido vida!')
-            ps_jugador -= 10
-            defensa_jugador -= 10
-            if defensa_jugador <= 0:
-                defensa_jugador = 1
+            atributo_jugador = atributo_jugador[0] - 10, atributo_jugador[1]
+            atributo_jugador = atributo_jugador[0], atributo_jugador[1] - 10
+            if atributo_jugador[1] <= 0:
+                atributo_jugador = atributo_jugador[0], 1
         elif ataque_oponente == 2:
             print('Squirtle ha usado placaje... Un ataque de tipo NORMAL')
             print('Charmander ha perdido vida!')
-            ps_jugador -= 35 * (defensa_jugador/100) #placaje
+            atributo_jugador = atributo_jugador[0] - 35 * (atributo_jugador[1]/100), atributo_jugador[1] #placaje
         elif ataque_oponente == 3:
             print('Squirtle ha usado Pistola Agua... Un ataque de tipo AGUA')
             print('Ataque crítico')
             print('Charmander ha perdido mucha vida!')
-            ps_jugador -= 40 #pistola de agua.
+            atributo_jugador = atributo_jugador[0] - 40, atributo_jugador[1] #pistola de agua.
         #Revisión de vida post turno
-        if ps_jugador < 0: 
-            ps_jugador = 0
+        if atributo_jugador[0] < 0: 
+            atributo_jugador = 0, atributo_jugador[1]
             print('Has perdido!')
-        elif ps_oponente < 0:
-            ps_oponente = 0
+        elif atributo_oponente[0] < 0:
+            atributo_oponente = 0, atributo_oponente[1]
             print('Has ganado!')
-        print(f'Charmader {ps_jugador}ps')
-        print(f'Squirtle {ps_oponente}ps')
+        print(f'Charmader {atributo_jugador[0]}ps')
+        print(f'Squirtle {atributo_oponente[0]}ps')
 elif elección == 2:
     print('Bien bello pues, te asustaste chico?')
     quit()
